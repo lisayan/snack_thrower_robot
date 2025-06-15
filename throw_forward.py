17ec144e88f53a96d9bcc9a9306ab479314c8937
Justin Jung <justinsoljung@gmail.com>
2025-06-14 19:44:26 -0700

    inter code

--- throw_forward.py
+++ throw_forward.py
@@ -4,17 +4,38 @@ import sys
 from lerobot.common.robots.so101_follower import SO101Follower
 from lerobot.common.robots.so101_follower import SO101FollowerConfig
 import time
+import numpy as np 
+
+VECTOR_TO_KEY = {1: 'shoulder_pan.pos', 2: 'shoulder_lift.pos', 3: 'elbow_flex.pos', 4: 'wrist_flex.pos', 5: "wrist_roll.pos", 6: "gripper.pos"}
 
 
-def main(robot, pull_back, flick_forward, gripper_open):
+def main(robot, pull_back, flick_forward, gripper_open, duration: float, n_points=10):
+    '''
+    Moves from pull_back position to flick forward position in duration (s)
+    '''
     current_obs = robot.get_observation()
-
-
+    # move to starting state 
     robot.send_action(pull_back)
-    time.sleep(1.0)
+    time.sleep(1)
+    
+    # forward trajectory 
+    start_vector = np.array([pull_back[VECTOR_TO_KEY[i]] for i in range(1,7)])
+    end_vector = np.array([flick_forward[VECTOR_TO_KEY[i]] for i in range(1,7)])
 
-    robot.send_action(flick_forward)
-    time.sleep(0.2)
+    all_inter_states = np.linspace(start_vector, end_vector, num=n_points, endpoint=True)
+    for j in range(0, len(all_inter_states)):
+        inter_state = {VECTOR_TO_KEY[i]: all_inter_states[j][i-1] for i in range(1,7) }
+
+        robot.send_action(inter_state)
+        time.sleep(duration / n_points)
+    
+    for i in range(5):
+        robot.send_action(flick_forward)
+        time.sleep(0.1)
+
+
+    #robot.send_action(flick_forward)
+    #time.sleep(0.2)
 
     #robot.send_action(gripper_open)
     #time.sleep(0.5)
@@ -74,7 +95,7 @@ if __name__ == "__main__":
     robot = SO101Follower(config)
     robot.connect(calibrate=False)
 
-    main(robot, PULL_BACK, TEST_FORWARD, RELEASE_GRIP)
+    main(robot, PULL_BACK, TEST_FORWARD, RELEASE_GRIP, duration=1)
 
 
 
